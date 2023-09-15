392dc672200abd536c9e7e44429f622a0ab5bc9e
Carmen Araya Rodríguez <carmenarayarodriguez@gmail.com>
2023-09-15 18:25:33 -0300

    Agrega ejemplo a dto

--- src/blog-y-noticias/dto/home-card.dto.ts
+++ src/blog-y-noticias/dto/home-card.dto.ts
@@ -2,16 +2,16 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CardDTO {
-    @ApiProperty({ description: 'ID de la tarjeta' })
+    @ApiProperty({ description: 'ID de la tarjeta', example: 'id-123' })
     @IsString()
     id: string;
 
-    @ApiProperty({ description: 'Título de la tarjeta' })
+    @ApiProperty({ description: 'Título de la tarjeta', example: 'Tarjeta Ejemplo' })
     @IsString()
     @IsNotEmpty()
     titulo: string;
 
-    @ApiProperty({ description: 'URL de la imagen de la tarjeta' })
+    @ApiProperty({ description: 'URL de la imagen de la tarjeta', example: 'https://ejemplo.com/imagen.jpg' })
     @IsString()
     @IsNotEmpty()
     imagenUrl: string;
