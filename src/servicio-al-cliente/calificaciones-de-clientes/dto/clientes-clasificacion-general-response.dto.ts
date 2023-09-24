2f3c5e4cf90d2f7656c61a46d3ad010f0daa232d
Carmen Araya Rodríguez <carmenarayarodriguez@gmail.com>
2023-09-24 11:50:41 -0300

    Agrega repuesta de back end a productos relacionados

---
+++ src/servicio-al-cliente/calificaciones-de-clientes/dto/clientes-clasificacion-general-response.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNumber, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { DetalleCalificacionGeneralResponseDTO } from './detalles-clasificacion-general-response.dto';
+import { ComentarioClienteResponseDTO } from './comentario-cliente-response.dto';
+import { ReaccionesAComentarioDTO } from './reacciones-a-comentario.response.dto';
+import { v4 as uuidv4 } from 'uuid';
+
+export class ClientesCalificacionGeneralResponseDTO {
+    @ApiProperty()
+    @IsString()
+    productoId: string = uuidv4();
+
+    @ApiProperty()
+    @IsNumber()
+    promedioEstrellas: number;
+
+    @ApiProperty()
+    @IsNumber()
+    numeroComentarios: number;
+
+}
