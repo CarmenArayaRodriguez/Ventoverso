447c843c6eaeaf350048978169e4cb732f38351b
Carmen Araya Rodríguez <carmenarayarodriguez@gmail.com>
2024-01-06 11:51:34 -0300

    Agrega rol de administrador a controller de marca y categorías

--- src/modules/marca.module.ts
+++ src/modules/marca.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Marca } from 'src/entities/marca.entity';
 import { MarcaService } from 'src/services/marca.service';
 import { MarcaController } from 'src/controllers/marca.controller';
+import { AutenticacionModule } from './autenticacion.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Marca])],
+    imports: [TypeOrmModule.forFeature([Marca]), AutenticacionModule],
     providers: [MarcaService],
     controllers: [MarcaController],
 })
